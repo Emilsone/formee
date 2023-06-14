1974cc5ba65782671448679d5155f37a1d283033
Your Name Here <lawsonezekiel68@gmail.com>
2023-06-14 13:50:26 +0100

    refix form file

--- src/components/NewsLetter.vue
+++ src/components/NewsLetter.vue
@@ -12,8 +12,7 @@
 					information
 					you
 					require. However, if you send the message now, our team will contact you right away</p>
-
-				<form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
+				<form name="contact" method="POST" netlify data-netlify="true" data-netlify-honeypot="bot-field">
 					<input type="hidden" name="form-name" value="Contact" />
 					<input type="text" name="name" placeholder="Name" />
 					<input type="email" name="email" placeholder="Email" />
